ae9c49fd3edb09cea86968b97e9298ed11641167
Borges <borgespaulo72@yahoo.com.br>
2026-05-27 15:55:17 -0300

    Iniciando o Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component/projects.component';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectsComponent],
+  imports: [RouterOutlet, ProjectsComponent, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
