106d390ab5415537412fe336ddb0a79b254c4e9b
Borges <borgespaulo72@yahoo.com.br>
2026-05-27 18:41:51 -0300

    feat: Iniciando about

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component/projects.component';
 import { Header } from './components/header/header';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectsComponent, Header],
+  imports: [RouterOutlet, ProjectsComponent, Header, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
