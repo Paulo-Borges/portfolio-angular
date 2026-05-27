bdbf62c0d9f0928dfc49fe5c140f391352e88b9c
Borges <borgespaulo72@yahoo.com.br>
2026-05-27 15:18:11 -0300

    feat: Incluindo imagem em construção

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -10,19 +10,17 @@ export class ProjectService {
       title: 'BorgEventos',
       description: 'Sistema completo de gerenciamento de eventos, com back-end robusto desenvolvido em .NET e APIs RESTful.',
       technologies: ['.NET', 'C#', 'SQL Server', 'Entity Framework'],
-      githubUrl: 'https://github.com/Paulo-Borges', // Substitua pelo link direto do repo se preferir
-      imageUrl: 'assets/images/borgeventos.png'
+      githubUrl: 'https://github.com/Paulo-Borges/BorgEventos.git',
+      imageUrl: 'emConstrucao.jpg'
     },
     {
       title: 'Ecarto',
-      description: 'Aplicação front-end moderna e responsiva construída para gerenciamento e interações dinâmicas de usuários.',
-      technologies: ['React', 'JavaScript', 'CSS3'], // Exemplo, adapte conforme o projeto
-      githubUrl: 'https://github.com/Paulo-Borges',
-      imageUrl: 'assets/images/ecarto.png'
+      description: 'Aplicação front-end, com meus projetos e de tudo que estou estudando.',
+      technologies: ['Angular 21', 'TailwindCss', 'BootStrap'], 
+      githubUrl: 'https://github.com/Paulo-Borges/portfolio-angular.git',
+      imageUrl: 'emConstrucao.jpg'
     }
   ];
-
-
 
   getProjects(): Project[] {
     return this.projects
